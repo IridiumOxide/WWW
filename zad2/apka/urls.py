97cfa7017b242fddcd4066c598ee2e702aca67d7
IridiumOxide <filip.chmielewski0@gmail.com>
2015-06-22 01:34:25 +0200

    Now with more AJAX

--- zad2/apka/urls.py
+++ zad2/apka/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<gmina_numer>[0-9]*)/$', views.gmina_menu, name='gmina_menu'),
-    url(r'^obwod/(?P<obwod_numer>[0-9]*)/$', views.obwod, name='obwod'),
+    url(r'^[0-9]*/obwod/(?P<obwod_numer>[0-9]*)/$', views.obwod, name='obwod'),
 ]
--- zad2/apka/views.py
+++ zad2/apka/views.py
@@ -1,8 +1,9 @@
 # Encoding: utf-8
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 from django.contrib import messages
 from django.db import transaction
+import json
 from django.core.urlresolvers import reverse
 from models import Gmina, Obwod
 
@@ -26,33 +27,61 @@ def obwod(request, obwod_numer):
     oid = int(obwod_numer)
 
     obwod = None
+    response_data = {}
 
-    try:
-        obwod = Obwod.objects.select_for_update().get(id=oid)
-        ok_stare = int(request.POST['ok_stare'])
-        ok = int(request.POST['ok'])
-        upr_stare = int(request.POST['upr_stare'])
-        upr = int(request.POST['upr'])
+    if request.method == 'GET':
+        try:
+            obwod = Obwod.objects.get(id=oid)
+            response_data['ok'] = obwod.otrzymanych_kart
+            response_data['upr'] = obwod.uprawnionych
+            response_data['status'] = "OK"
+        except Obwod.DoesNotExist:
+            response_data['status'] = "OBWOD_NOT_FOUND"
+            # messages.error(request, "Obwod not found")
 
-        if ok < 0 or upr < 0:
-            messages.error(request, "Nie można wprowadzać ujemnych liczb")
-        elif ok_stare != obwod.otrzymanych_kart:
-            messages.error(request, "Dane zostały zmienione od ostatniego odczytu."
-                                    "Zapisujesz %d, a ktoś inny zapisał %d" % (ok, obwod.otrzymanych_kart))
-        elif upr_stare != obwod.uprawnionych:
-            messages.error(request, "Dane zostały zmienione od ostatniego odczytu."
-                                    "Zapisujesz %d, a ktoś inny zapisał %d" % (upr, obwod.uprawnionych))
-        else:
-            obwod.otrzymanych_kart = ok
-            obwod.uprawnionych = upr
-            obwod.save()
-            messages.success(request, "Dane zostały zapisane.")
+    elif request.method == 'POST':
+        try:
+            obwod = Obwod.objects.select_for_update().get(id=oid)
+            ok_stare = int(request.POST['ok_stare'])
+            ok = int(request.POST['ok'])
+            upr_stare = int(request.POST['upr_stare'])
+            upr = int(request.POST['upr'])
 
-    except ValueError:
-        messages.error(request, "Błędne dane")
-    except KeyError:
-        messages.error(request, "Niekompletne dane")
-    except Obwod.DoesNotExist:
-        messages.error(request, "Obwod not found")
+            if ok < 0 or upr < 0:
+                response_data['status'] = "NEGATIVE_NUMBERS"
+                # messages.error(request, "Nie można wprowadzać ujemnych liczb")
+            elif ok_stare != obwod.otrzymanych_kart:
+                response_data['status'] = "DATA_CHANGED"
+                response_data['original'] = obwod.otrzymanych_kart
+                response_data['new'] = ok
+                # messages.error(request, "Dane zostały zmienione od ostatniego odczytu."
+                #                        "Zapisujesz %d, a ktoś inny zapisał %d" % (ok, obwod.otrzymanych_kart))
+            elif upr_stare != obwod.uprawnionych:
+                response_data['status'] = "DATA_CHANGED"
+                response_data['original'] = obwod.uprawnionych
+                response_data['new'] = upr
+                # messages.error(request, "Dane zostały zmienione od ostatniego odczytu."
+                #                        "Zapisujesz %d, a ktoś inny zapisał %d" % (upr, obwod.uprawnionych))
+            else:
+                obwod.otrzymanych_kart = ok
+                obwod.uprawnionych = upr
+                obwod.save()
+                # messages.success(request, "Dane zostały zapisane.")
+                response_data['ok'] = obwod.otrzymanych_kart
+                response_data['upr'] = obwod.uprawnionych
+                response_data['status'] = "OK"
 
-    return HttpResponseRedirect(reverse('gmina_menu', args=(obwod.gmina.numer,)))
+        except ValueError:
+            response_data['status'] = "WRONG_DATA"
+            # messages.error(request, "Błędne dane")
+        except KeyError:
+            response_data['status'] = "INCOMPLETE DATA"
+            # messages.error(request, "Niekompletne dane")
+        except Obwod.DoesNotExist:
+            response_data['status'] = "OBWOD_NOT_FOUND"
+            # messages.error(request, "Obwod not found")
+
+    return HttpResponse(
+        json.dumps(response_data),
+        content_type="application/json"
+    )
