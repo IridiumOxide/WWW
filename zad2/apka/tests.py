ee8741b519f3f8395eecf1a65156745871903a7b
IridiumOxide <filip.chmielewski0@gmail.com>
2015-06-23 02:41:09 +0200

    Added selenium test

--- zad2/apka/tests.py
+++ zad2/apka/tests.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.test import LiveServerTestCase
 from selenium.webdriver.firefox.webdriver import WebDriver
 from models import Obwod, Gmina
@@ -24,5 +26,46 @@ class SeleniumTest(LiveServerTestCase):
         cls.selenium.quit()
         super(SeleniumTest, cls).tearDownClass()
 
-    def test_edit(self):
-        self.selenium.get('%s%s' % (self.live_server_url, 'apka/1/'))
+    def test_overwrite(self):
+        driver2 = WebDriver()
+        self.selenium.get('%s%s' % (self.live_server_url, '/apka/1/'))
+        self.selenium.implicitly_wait(100)
+        edit_button1 = self.selenium.find_element_by_id('editButton1')
+        save_button1 = self.selenium.find_element_by_id('submitButton1')
+        edit_button1.click()
+
+        driver2.get('%s%s' % (self.live_server_url, '/apka/1/'))
+        driver2.implicitly_wait(100)
+        edit_button2 = driver2.find_element_by_id('editButton1')
+        save_button2 = driver2.find_element_by_id('submitButton1')
+        edit_button2.click()
+        driver2.implicitly_wait(100)
+        inputok = driver2.find_element_by_name('ok')
+        inputok.clear()
+        inputok.send_keys('0')
+        inputupr = driver2.find_element_by_name('upr')
+        inputupr.clear()
+        inputupr.send_keys('1')
+        save_button2.click()
+        alert = driver2.switch_to.alert
+        self.assertEqual(alert.text, "Zapisano dane")
+        alert.accept()
+        obwod = Obwod.objects.get(id=1)
+        self.assertEqual(obwod.otrzymanych_kart, 0)
+        self.assertEqual(obwod.uprawnionych, 1)
+        driver2.quit()
+
+        self.selenium.implicitly_wait(100)
+        inputok = self.selenium.find_element_by_name('ok')
+        inputok.clear()
+        inputok.send_keys('42')
+        inputupr = self.selenium.find_element_by_name('upr')
+        inputupr.clear()
+        inputupr.send_keys('1337')
+        save_button1.click()
+        alert = self.selenium.switch_to.alert
+        self.assertEqual(alert.text, u"Dane zmieniły się od ostatniego zapisu. Zapisujesz 1337, a ktoś inny zapisał 1")
+        alert.accept()
+        obwod = Obwod.objects.get(id=1)
+        self.assertEqual(obwod.otrzymanych_kart, 0)
+        self.assertEqual(obwod.uprawnionych, 1)
---
+++ zad2/static_media/scripts.js
@@ -0,0 +1,147 @@
+var csrftoken = $.cookie('csrftoken');
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
+
+function submitForm(id){
+    console.log("kinda submitted the form");
+    var d_ok = $("input[name='ok']").val();
+    var d_ok_stare = $("input[name='ok_stare']").val();
+    var d_upr = $("input[name='upr']").val();
+    var d_upr_stare = $("input[name='upr_stare']").val();
+    console.log("sending OK:  " + d_ok);
+    console.log("sending UPR: " + d_upr);
+    console.log("old OK:      " + d_ok_stare);
+    console.log("old UPR:     " + d_upr_stare);
+    $.ajax({
+        url : 'obwod/' + id + '/',
+        type: "POST",
+        data: {
+            ok: d_ok,
+            ok_stare: d_ok_stare,
+            upr: d_upr,
+            upr_stare: d_upr_stare
+        },
+        success: function(json){
+            console.log("success");
+            if(json.status != "OK" && json.status != "DATA_CHANGED_OK" && json.status != "DATA_CHANGED_UPR"){
+                $("#ok" + id).html(d_ok_stare);
+                $("#upr" + id).html(d_upr_stare);
+            }
+
+            if(json.status == "OK") {
+                console.log("sukces bardzo");
+                $("#ok" + id).html(json.ok);
+                $("#upr" + id).html(json.upr);
+                alert("Zapisano dane");
+            }else if(json.status == "OBWOD_NOT_FOUND"){
+                console.log("obwod 404 :<");
+                alert("Nie ma takiego obwodu");
+            }else if(json.status == "WRONG_DATA"){
+                console.log("bad data");
+                alert("Błędne dane");
+            }else if(json.status == "INCOMPLETE_DATA"){
+                console.log("incomplete data");
+                alert("Niekompletne dane");
+            }else if(json.status == "NEGATIVE_NUMBERS"){
+                console.log("negative numbers");
+                alert("Nie można wprowadzać ujemnych liczb");
+            }else if(json.status == "DATA_CHANGED") {
+                console.log("data change");
+                getDataCancel(id);
+                alert("Dane zmieniły się od ostatniego zapisu. Zapisujesz " + json.new + ", a ktoś inny zapisał " + json.original);
+            }
+            $("#editButton" + id).show();
+            $("#submitButton" + id).hide();
+            $("#cancelButton" + id).hide();
+        },
+        error: function(xhr, errmsg){
+            console.log("error");
+            console.log(xhr.status + ": " + xhr.responseText + "\n" + errmsg);
+            alert("An error has happened to happen.")
+        }
+    });
+}
+
+
+function getDataEdit(id){
+    console.log("Gettin' data");
+    $.ajax({
+        url : 'obwod/' + id + '/',
+        type: "GET",
+        success: function(json){
+            console.log("Got data: " + json.ok + " , " + json.upr);
+            $("#ok" + id).replaceWith('<td id="ok' + id + '"><input type="hidden" name="ok_stare" value="' + json.ok + '" form="form' + id + '"><input type="text" name="ok" value="' + json.ok + '" form="form' + id + '"></td>');
+            $("#upr" + id).replaceWith('<td id="upr' + id + '"><input type="hidden" name="upr_stare" value="' + json.upr + '" form="form' + id + '"><input type="text" name="upr" value="' + json.upr + '" form="form' + id + '"></td>');
+            $("#editButton" + id).hide();
+            $("#submitButton" + id).show();
+            $("#cancelButton" + id).show();
+        },
+        error: function(xhr, errmsg){
+            console.log("error");
+            console.log(xhr.status + ": " + xhr.responseText + "\n" + errmsg);
+            alert("An error has happened to happen.")
+        }
+    })
+}
+
+
+function getDataCancel(id){
+    console.log("Gettin' data");
+    $.ajax({
+        url : 'obwod/' + id + '/',
+        type: "GET",
+        success: function(json){
+            console.log("Got data: " + json.ok + " , " + json.upr);
+            $("#ok" + id).html(json.ok);
+            $("#upr" + id).html(json.upr);
+            $("#editButton" + id).show();
+            $("#submitButton" + id).hide();
+            $("#cancelButton" + id).hide();
+        },
+        error: function(xhr, errmsg){
+            console.log("error");
+            console.log(xhr.status + ": " + xhr.responseText + "\n" + errmsg);
+            alert("An error has happened to happen.")
+        }
+    })
+}
+
+
+function zmiana(id) {
+    console.log("editButton" + id);
+    var subB = $("#submitButton" + id);
+    var canB = $("#cancelButton" + id);
+    var ediB = $("#editButton" + id);
+    $(document).ready(subB.hide());
+    $(document).ready(canB.hide());
+    $(document).ready($("#form" + id).on('submit', function(event){
+        event.preventDefault();
+        submitForm(id);
+    }));
+    $(document).ready(ediB.click(function () {
+        console.log("klik " + id);
+        getDataEdit(id);
+        return false;
+    }));
+    $(document).ready(subB.click(function () {
+        console.log("klik zapisz");
+        $("#form" + id).submit();
+        return false;
+    }));
+    $(document).ready(canB.click(function () {
+        console.log("klik anuluj");
+        getDataCancel(id);
+        return false;
+    }));
+}
